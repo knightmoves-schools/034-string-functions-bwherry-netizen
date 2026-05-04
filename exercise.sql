877bc86a1a829a2e6d4b13cbc4b4047f0fe2bc35
Brendon W. <bwherry@midland.k12.ia.us>
2026-05-04 12:05:47 -0500

    Add SQL query to select employee details
    Query retrieves employee details with initials and corrected location.

--- exercise.sql
+++ exercise.sql
@@ -1,1 +1,5 @@
--- Add your SQL here
+SELECT * FROM EMPLOYEE
+WHERE SUBSTRING(FIRST_NAME, 1, 1) as "First Initial"
+AND (SUBSTRING(LAST_NAME, 1, 1) as "Last Initial")
+And (SUBSTRING(DATE(DATE) 1, 4) as "Birth Year")
+AND (UPPER(TRIM(LOCATION)) as "Corrected Location")
